8ae1e09ac77e29302b48cc722f0d85ed2fd2b839
m2carreira <mariana.carreira.work@gmail.com>
2026-05-04 17:17:41 +0100

    criaçao do script e correções de algum codigo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+    toggle.addEventListener('click', () =>{
+
+        // Adiciona o mostrar o menu na nav
+        nav.classList.toggle('show-menu')
+
+        //Adiciona o mostrar o icon para mostrar e esconde o menu icon
+        toggle.classList.toggle('show-icon')
+    
+    })
+}
+showMenu('nav-toggle','navMenu')
